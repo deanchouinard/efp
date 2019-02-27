d217b0a60115b5183dec3f455117ea1325bbf04a
Dean Chouinard <deanchouinard@gmail.com>
2019-02-26 21:24:17 -0500

    an initial server

--- gallows/lib/gallows_web/router.ex
+++ gallows/lib/gallows_web/router.ex
@@ -17,6 +17,9 @@ defmodule GallowsWeb.Router do
     pipe_through :browser
 
     get "/", HangmanController, :new_game
+
+    post "/", HangmanController, :create_game
+
   end
 
   # Other scopes may use custom stacks.
--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,3 +1,6 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+
+  def word_so_far(letters), do: letters |> Enum.join(" ")
+
 end
